71e84f5786b6a663ad06253b6db0da669edac7b3
Julia Puschnerat <jupuschnerat@gmail.com>
2023-11-27 17:39:29 -0300

    start controllers

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,2 +1,28 @@
 class DestinationsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :show ]
+  before_action :set_destination, only: [:show]
+
+  # as a user I can check all the destinations of my search
+  # GET /destinations
+  def index
+    @destinations = destinations.all
+  end
+
+  # as a user I can see more detailes of an specific destination
+  # GET /destinations/:destination_id
+  def show
+
+  end
+
+
+
+  private
+
+  def set_destination
+    @destination = destination.find(params[:destination_id])
+  end
+
+  def destination_params
+    params.require(:destination).permit()
+  end
 end

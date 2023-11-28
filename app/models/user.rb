7a3b9333ca5858c9588a9a41980f5c137ccaf2fd
Julia Puschnerat <jupuschnerat@gmail.com>
2023-11-28 11:55:17 -0300

    models users

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
   has_many :routes
   has_many :bookings
   validates_length_of :password, in: 6..20, on: :create
+  validates :full_name, :phone_number, :birth_date, :email, :password, :address, presence: true
 end

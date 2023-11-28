40e20ef4a3b281f1dde8b0fe8f276d531461fb99
Julia Puschnerat <jupuschnerat@gmail.com>
2023-11-28 11:45:21 -0300

    controllers

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -3,46 +3,21 @@ class RoutesController < ApplicationController
 
   # as a user I can check all the routes of my search
   # GET /routes
-  def index
-    @routes = Routes.all
-    # search by city?
-  end
+
 
   # as a user I can see more detailes of an specific route
   # GET /routes/:route_id
-  def show
 
-  end
 
-  # as a user I can create a new route
+  # as a user I can search?create? a new route
   # GET routes/new
-  def new
-    @route = Routes.new
-  end
 
-  # as a user I can create a new route
+
+  # as a user I can search?create a new route
   # POST /routes
-  def create
-    @route = Route.new(route_params)
-    @route.user = current_user
-  end
 
-  # PATCH/PUT /routes/:route_id
-  def update
 
-  end
 
-  # GET /routes/:route_id/edit
-  # as I user I can edit a route
-  def edit
-
-  end
-
-  # DELETE /routes/:route_id
-  # as a user I can destroy a route
-  def destroy
-
-  end
 
   private
 

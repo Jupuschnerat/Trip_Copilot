74af83eeb589372f4a106eb1f984df00c0e9c419
Rodrigo Ramos <rodrigo.sr@hotmail.com>
2023-11-30 14:21:36 -0300

    homepage almost

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,6 +5,18 @@ import "@popperjs/core"
 import "bootstrap"
 //= require moment
 //= require bootstrap-datepicker
-import flatpickr from "flatpickr"
-import "flatpickr/dist/flatpickr.min.css"
-flatpickr(".datepicker", {})
+import { Application } from 'stimulus'
+import { definitionsFromContext } from 'stimulus/webpack-helpers'
+
+const application = Application.start()
+const context = require.context('../controllers', true, /\.js$/)
+application.load(definitionsFromContext(context))
+
+// import Flatpickr
+import Flatpickr from 'stimulus-flatpickr'
+
+// Import style for flatpickr
+require("flatpickr/dist/flatpickr.css")
+
+// Manually register Flatpickr as a stimulus controller
+application.register('flatpickr', Flatpickr)

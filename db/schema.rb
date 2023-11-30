7994a2188a05fee14304d8b4c0691dee4bc1ebac
Rodrigo Ramos <rodrigo.sr@hotmail.com>
2023-11-30 16:06:43 -0300

    homepage done

--- db/schema.rb
+++ db/schema.rb
@@ -79,7 +79,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_30_160944) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.text "address", default: "", null: false
-    t.string "image", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"

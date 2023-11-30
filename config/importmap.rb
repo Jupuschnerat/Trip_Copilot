68a1858527ca1c831f40df8dfeb89d954fb24bfb
Julia Puschnerat <jupuschnerat@gmail.com>
2023-11-30 14:29:28 -0300

    changes

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,7 +8,4 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
 pin "flatpickr", to: "https://ga.jspm.io/npm:flatpickr@4.6.13/dist/esm/index.js"
-<<<<<<< HEAD
-=======
 pin "stimulus-flatpickr", to: "https://ga.jspm.io/npm:stimulus-flatpickr@3.0.0-0/dist/index.m.js"
->>>>>>> cd561892261ef36dc6329964d59e73849f52fe4a

66c48cbdd68ce3de7dbeb6ac46878dc9a4b69d80
lucasprado <lucaspradobr@gmail.com>
2023-11-30 18:52:06 -0300

    route builder with no loop

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,5 +1,6 @@
 require "json"
 require "open-uri"
+require "net/http"
 
 class RoutesController < ApplicationController
   before_action :set_route, only: [:show]
@@ -31,7 +32,9 @@ class RoutesController < ApplicationController
     @route.user = current_user
     @route.save
 
-    # Get the toke
+
+
+    # Get the token
     uri = URI.parse("https://test.api.amadeus.com/v1/security/oauth2/token")
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
@@ -44,14 +47,11 @@ class RoutesController < ApplicationController
 
     api_return_json = JSON.parse(response.body)
 
-    p api_return_json["access_token"]
-
+    # get the cheapest flight
     base_url = "https://test.api.amadeus.com/v1/shopping/flight-destinations?origin=#{@route.departure_place}&maxPrice=#{@route.budget.floor}"
     headers = {
       'Authorization' => "Bearer #{api_return_json["access_token"]}"
     }
-
-
     url = base_url
     api_return = URI.open(url, headers).read
     api_return_json = JSON.parse(api_return)
@@ -59,9 +59,8 @@ class RoutesController < ApplicationController
     ordered_flights = flights.sort_by { |flight| flight['price']['total']}
     cheapest_flight = ordered_flights[0]
     origin = cheapest_flight["origin"]
-    p cheapest_flight
 
-    #creating destinations
+    # block to be removed when using the loop
     @destination = Destination.new()
     @destination.price = (cheapest_flight["price"]["total"].to_i)/2
     @destination.transportation = "plane"
@@ -69,13 +68,69 @@ class RoutesController < ApplicationController
     @destination.arrival_date = cheapest_flight["departureDate"]
     @destination.departure_city = cheapest_flight["origin"]
     @destination.arrival_city = cheapest_flight["destination"]
-
     @destination.route = @route
     @destination.save
 
-    #updating budget
+
+    budget = @route.budget
+    @route.total_price = 0
 
 
+
+    # if budget > (cheapest_flight["price"]["total"].to_i)/2
+    #   while budget > (cheapest_flight["price"]["total"].to_i)/2
+    #   #creating destinations
+    #     @destination = Destination.new()
+    #     @destination.price = (cheapest_flight["price"]["total"].to_i)/2
+    #     @destination.transportation = "plane"
+    #     @destination.departure_day = cheapest_flight["departureDate"]
+    #     @destination.arrival_date = cheapest_flight["departureDate"]
+    #     @destination.departure_city = cheapest_flight["origin"]
+    #     @destination.arrival_city = cheapest_flight["destination"]
+    #     @destination.route = @route
+    #     @destination.save
+
+    #     #update budget and route total price
+    #     budget -= @destination.price
+    #     @route.total_price += @destination.price
+
+    #     #updating api call
+
+    #     # Get the token
+    #     uri = URI.parse("https://test.api.amadeus.com/v1/security/oauth2/token")
+    #     http = Net::HTTP.new(uri.host, uri.port)
+    #     http.use_ssl = true
+
+    #     request = Net::HTTP::Post.new(uri.request_uri)
+    #     request['Content-Type'] = 'application/x-www-form-urlencoded'
+    #     request.body = "grant_type=client_credentials&client_id=#{ENV["AMADEUS_CLIENT_ID"]}&client_secret=#{ENV["AMADEUS_CLIENT_SECRET"]}"
+    #     response = http.request(request)
+    #     api_return_json = JSON.parse(response.body)
+
+    #     # Make the call again
+    #     base_url = "https://test.api.amadeus.com/v1/shopping/flight-destinations?origin=#{@destination.arrival_city}&maxPrice=#{budget.floor}"
+
+    #     headers = {
+    #       'Authorization' => "Bearer #{api_return_json["access_token"]}"
+    #     }
+    #     sleep 10
+    #     url = base_url
+    #     conn = Faraday.new(
+    #       headers: headers
+    #     )
+    #     response = conn.get(base_url)
+    #     debugger
+    #     # api_return = URI.open(url, headers).read
+    #     api_return_json = JSON.parse(response.body)
+    #     flights = api_return_json["data"]
+    #     ordered_flights = flights.sort_by { |flight| flight['price']['total']}
+    #     cheapest_flight = ordered_flights[0]
+    #     origin = cheapest_flight["origin"]
+
+    #   end
+    # else
+    #   p 'No routes for you.'
+    # end
 
     redirect_to route_path(@route)
 

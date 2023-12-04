efc6e361ff8de2815cbea389060a3b6ce3ad1830
Julia Puschnerat <jupuschnerat@gmail.com>
2023-12-04 16:15:10 -0300

    favorites

--- db/migrate/20231204173423_create_favorites.rb
+++ db/migrate/20231204173423_create_favorites.rb
@@ -1,4 +1,4 @@
-class CreateSavedRoutes < ActiveRecord::Migration[7.1]
+class CreateFavorites < ActiveRecord::Migration[7.1]
   def change
     create_table :saved_routes do |t|
       t.references :user, null: false, foreign_key: true

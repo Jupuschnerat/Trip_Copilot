e3321cd6c6d3e1917d669073da9a9ee2030a3e26
lucasprado <lucaspradobr@gmail.com>
2023-12-06 18:51:32 -0300

    multi-step form

--- app/controllers/intake/departure_places_controller.rb
+++ app/controllers/intake/departure_places_controller.rb
@@ -8,8 +8,9 @@ module Intake
       @departure_place = Intake::DeparturePlace.new(departure_place_params)
       if @departure_place.valid?
         session[:departure_place] = {
-          'departure_place' => @departure_place
+          'departure_place' => @departure_place.departure_place
         }
+
         redirect_to new_intake_budget_path
       else
         render :new, status: :unprocessable_entity
